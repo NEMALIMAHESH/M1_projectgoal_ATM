c3f249df58174a27067335aba20dd3d9e09c2a83
NemaliMahesh <nemalimahesh12@gmail.com>
2022-02-10 20:23:56 +0530

    mahesh

---
+++ 3_Implementation/ATM.h
@@ -0,0 +1,18 @@
+/**
+ * @file ATM.c
+ * @author your name (you@domain.com)
+ * @brief Example to learn Atm withdrawal, pin generate, statementrequest, billpayment, donation and balance enquiry
+ * @version 0.1
+ * @date 2022-02-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+// Header gaurd
+#ifndef atm_h_
+#define atm_h_
+
+#include"stdio.h"
+#include"conio.h"
+
+#endif
--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,16 +1,4 @@
-/**
- * @file ATM.c
- * @author your name (you@domain.com)
- * @brief Example to learn Atm withdrawal, pin generate, statementrequest, billpayment, donation and balance enquiry
- * @version 0.1
- * @date 2022-02-09
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-
-#include"stdio.h"
-#include"conio.h"
+#include"ATM.h"
 
 int atm(int p)
 {
